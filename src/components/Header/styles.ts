468edbdb23acde3b8bce8e90284b4439706f9056
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2023-08-20 22:14:42 -0300

    feat: add Header component

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,10 @@
+import { styled } from 'styled-components';
+
+export const Container = styled.header`
+	height: 60px;
+	background-color: #7159C1;
+	color: #FFF;
+	display: flex;
+	align-items: center;
+	padding: 0 30px;
+`;
